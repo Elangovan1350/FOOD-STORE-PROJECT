4ba5a32d72f247647bd9c907d1dd81361df14b23
Elangovan1350 <elangovan2019miss@gmail.com>
2025-08-28 09:18:03 +0530

    mouse effect

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import { CustomEase } from "gsap/CustomEase";
 
 gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger, customElements);
 
+const bottonAll = document.querySelectorAll("button");
+
+// image change animation
 const tl1 = gsap.timeline({ repeat: -1 });
 tl1.to(".gift", {
   scale: 1.1,
@@ -21,20 +24,47 @@ tl1.to(".gift", {
   ease: "power1.inOut(1.7 , 0.3)",
 });
 
+//  transition animation
+
 gsap.set(".wholePage", { display: "none" });
 
 const tl2 = gsap.timeline();
-gsap.set(".transition-box", { rotate: 180 });
+gsap.set(".transition-box", { rotate: 90 });
 tl2.from(".transition-box", {
-  y: "-60dvh",
+  x: "-120dvh",
   duration: 1,
+  delay: 0.2,
+  ease: "power1.inOut",
+});
+tl2.to(
+  ".to",
+  {
+    x: "70dvw",
+    duration: 1,
+    ease: "power1.inOut",
+  },
+  "-=0.22"
+);
+tl2.to(".transition-box", {
+  rotate: 180,
+  duration: 0.5,
   ease: "elastic.out(0.9, 0.7)",
 });
+
 tl2.to(".transition-box", {
   y: "38dvh",
-  ease: "elastic.out(0.9, 0.7)",
+  ease: "power1.inOut",
   duration: 1,
 });
+tl2.to(
+  ".foodMunch",
+  {
+    y: "60vh",
+    duration: 1,
+    ease: "power1.inOut",
+  },
+  "<+0.12"
+);
 tl2.to(".transition-box", {
   rotate: 0,
   ease: "elastic.out(0.9, 0.7)",
@@ -86,8 +116,37 @@ tl2.to(
   "<-0.5"
 );
 
+// navbar animation
+
 tl2.from(".navbar1", {
   y: "-10dvh",
   duration: 1,
   ease: "elastic.out(0.9, 0.7)",
+});
+
+bottonAll.forEach((ele) => {
+  ele.addEventListener("mouseover", () => {
+    gsap.to(ele, {
+      scale: 1.1,
+      duration: 0.2,
+    });
+  });
+  ele.addEventListener("mouseout", () => {
+    gsap.to(ele, {
+      scale: 1,
+      duration: 0.2,
+    });
+  });
+  ele.addEventListener("mousedown", () => {
+    gsap.to(ele, {
+      scale: 0.9,
+      duration: 0.1,
+    });
+  });
+  ele.addEventListener("mouseup", () => {
+    gsap.to(ele, {
+      scale: 1,
+      duration: 0.1,
+    });
+  });
 });
